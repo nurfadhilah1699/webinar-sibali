f18a814371e46e94fa0dd9afd6e66b76916d277e
Nurfadhilah <nurfadhilahalang480@gmail.com>
2026-01-28 19:05:59 +0800

    Add favicon link to admin layout

--- resources/views/components/admin-layout.blade.php
+++ resources/views/components/admin-layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel - Webinar Sibali</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-50 antialiased">
